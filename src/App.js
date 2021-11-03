22cc9c4ebf89b1c1f0ee0e252410899f07f06e4c
이무원 <whitewind@imuwon-ui-iMac.local>
2021-11-03 22:06:23 +0900

    env

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import axios from "axios";
 import { useState } from 'react';
+import io from 'socket.io-client';
 
-const serverDomain = 'https://mususerver.herokuapp.com/'
+// const serverDomain = 'https://mususerver.herokuapp.com/'
+const serverDomain = process.env.REACT_IO_SERVER || 'http://localhost:3001';
+const socket = io(serverDomain);
 
 function App() {
   const [servermessage, setServermesage] = useState('');
+  const [socketmessage, setSocketmesage] = useState('');
   axios(serverDomain)
     .then(res=>{
       console.log(res.data);
       setServermesage(res.data);
     })
-  
+  function handleClick(e){
+    e.preventDefault();
+    socket.emit('welcome', (data)=>{
+    setSocketmesage(data);
+    })
+  };  
   
   return (
     <>
       <h1>{servermessage}</h1>
       this is MuSU Client
+      <button onClick={handleClick}></button>
+      <h2>{socketmessage}</h2>
     </>
   );
 }
