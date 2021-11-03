64f018211676d103955fd748d2f8162278426628
이무원 <whitewind@imuwon-ui-iMac.local>
2021-11-03 22:12:13 +0900

    env2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import io from 'socket.io-client';
 
 // const serverDomain = 'https://mususerver.herokuapp.com/'
-const serverDomain = process.env.REACT_IO_SERVER || 'http://localhost:3001';
+const serverDomain = process.env.REACT_APP_IO || 'http://localhost:3001';
 const socket = io(serverDomain);
 
 function App() {
